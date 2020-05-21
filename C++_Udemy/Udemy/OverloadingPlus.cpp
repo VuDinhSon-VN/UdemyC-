d0ef30d9ba3c50c930b3ffa29b86f71a00be1e63
sonvudinh96@gmail.com <dinhson.vu@samsung.com>
2020-05-21 14:59:59 +0700

    upadte file command

---
+++ C++_Udemy/Udemy/OverloadingPlus.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include "Complex.h"
+//
+//using namespace std;
+//using namespace caveofprogramming;
+//
+//int main() {
+//
+//	Complex c1(3, 4);
+//	Complex c2(2, 3);
+//	cout << c1+c2 << endl;
+//
+//	Complex c3(4, 2);
+//	cout << c3 + 7 << endl;
+//	cout << 7 + c3 << endl;
+//
+//	return 0;
+//
+//}
---
+++ C++_Udemy/Udemy/Overloading_Equalit_Test.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include "Complex.h"
+//
+//using namespace std;
+//using namespace caveofprogramming;
+//
+//int main() {
+//
+//	Complex c1(3, 4);
+//	Complex c2(3, 4);
+//	
+//	if (c1 == c2)
+//	{
+//		cout << "Equal" << endl;
+//	}
+//	else cout << "Not equal" << endl;
+//
+//	if (c1 != c2)
+//	{
+//		cout << "Not equal" << endl;
+//	}
+//	else cout << "Equal" << endl;
+//	return 0;
+//
+//}
