200f060072c5cb06854621cd9401daaac0faf73c
vudinh_son <dinhson.vu@samsung.com>
2020-05-21 08:00:21 +0700

    syn system

---
+++ C++_Udemy/Udemy/Two-Demensional Vector.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main() {
+//
+//	vector< vector<int> > gird(3, vector<int>(4, 7));
+//
+//	cout << gird[1].capacity() << endl;
+//	
+//
+//	for (int row = 0; row < gird.size(); row++) {
+//		for (int col = 0; col < gird[row].size(); col++)
+//		{
+//			cout << gird[row][col] << flush;
+//		}
+//		cout << endl;
+//	}
+//
+//
+//	
+//	return 0;
+//}
